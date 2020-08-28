e1fa6bd36f5daa93061fae6c70b6393589684f8a
margare-jihua <margaret.jihua@gmail.com>
2020-08-28 14:07:39 -0700

    [Routes] Set up a few routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+}from 'react-router-dom'
+
+import Home from './Home'
+import Blog from './Blog'
+import About from './About'
+import Projects from './Projects'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to='/'>Home</Link>{'  '}
+          <Link to='/blog'>Blog</Link>{'  '}
+          <Link to='/about'>About</Link>{'  '}
+          <Link to='/projects'>Projects</Link>{'  '}
+        </nav>
+        <Route exact path='/' component={Home} />
+        <Route path='/blog' component={Blog} />
+        <Route path='/about' component={About} />
+        <Route path='/projects' component={Projects} />
+      </div>
+    </Router>
+    
   );
 }
 
