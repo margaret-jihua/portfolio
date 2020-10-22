9cd2cb1adf5c7a9e5169953de1e514a41ea46687
margare-jihua <margaret.jihua@gmail.com>
2020-10-21 23:04:25 -0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import { BrowserRouter as Router, Route, Link }from 'react-router-dom'
 
 import Nav from './components/Nav'
 import Home from './components/Home'
-import Blog from './components/Blog'
 import Weather from './Weather'
 
 function App() {
-
-  const posts = [
-    {title: 'GA Software Engineering Immersive'}
-  ]
 
   return (
     <Router>
       <div className="App">        
         <Nav />
         <Route exact path='/' component={Home} />
-        <Route path='/blog' render={ () => <Blog posts={this.props.posts} /> }/>
         {/* <Route path='/about' component={About} />
         <Route path='/projects' render={() => <Projects projects={projects} />} /> */}
         <Route path='/weather' render={() => <Weather /> }/>
+        <footer className="text-center" style={{marginTop: "100px"}}>Design & Build by Margaret (Jihua) Huang</footer>
       </div>
     </Router>
     
