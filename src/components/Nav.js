265bab52bd5d21b53ae3ee8ae05379f3306dd1e7
margare-jihua <margaret.jihua@gmail.com>
2020-10-25 20:35:58 -0700

    make nav bar float to the side

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,23 +3,28 @@ import { NavLink, Link } from 'react-router-dom';
 
 export default function Nav(props) {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <Link class="navbar-brand" to='/'>Home</Link>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#projects">Projects</a>
-                    </li>
-                </ul>
-            </div>
+        <nav class="bar d-flex flex-column">
+            <a class="" href="#about">About</a>
+            <a class="" href="#project">Project</a>
+            <a class="" href="#contact">Contact</a>
         </nav>
+        // <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
+        //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span class="navbar-toggler-icon"></span>
+        //     </button>
+        //     <Link class="navbar-brand" to='/'>Home</Link>
+
+        //     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        //         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        //             <li class="nav-item">
+        //                 <a class="nav-link" href="#about">About</a>
+        //             </li>
+        //             <li class="nav-item">
+        //                 <a class="nav-link" href="#projects">Projects</a>
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </nav>
     )
 }
 {/* <nav>
