725de7fbc3f120eab75da76084ed138afdbd9d8e
margare-jihua <margaret.jihua@gmail.com>
2020-10-25 21:30:26 -0700

    Add instagram link in contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
+import lucas from '../assets/lucas.png'
+import lily from '../assets/lily.png'
+import instagram from '../assets/instagram.png'
 
 export default class Contact extends Component {
     render() {
         return (
-            <>
-            <h1 className="heading">Let's get in touch</h1>
-            </>
+            <div id="contact" className="container text-center">
+                <h1 className="heading">Let's get in touch</h1>
+                <h4>✨😺come check out my cats😸✨</h4>
+                <img src={lucas} height="48" />
+                <img src={lily} alt="Lily" height="48" />
+                <a href="https://www.instagram.com/walklikeacat"><img src={instagram} height="62" /></a>
+            </div>
         )
-
     }
 }
