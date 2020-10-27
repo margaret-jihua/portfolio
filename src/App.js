0d269c7fb5810e8707e961df955f46d57562e701
margare-jihua <margaret.jihua@gmail.com>
2020-10-27 16:25:32 -0700

    Edit Contact Section and Add google fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './style.css';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Link }from 'react-router-dom'
@@ -15,10 +14,7 @@ function App() {
       <div className="App">        
         <Nav />
         <Route exact path='/' component={Home} />
-        {/* <Route path='/about' component={About} />
-        <Route path='/projects' render={() => <Projects projects={projects} />} /> */}
         <Route path='/weather' render={() => <Weather /> }/>
-        <footer className="text-center" style={{marginTop: "100px"}}>Design & Build by Margaret (Jihua) Huang</footer>
       </div>
     </Router>
     
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react'
 import lucas from '../assets/lucas.png'
 import lily from '../assets/lily.png'
-import instagram from '../assets/instagram.png'
+import instagram from '../assets/icons8-instagram-60.png'
+import github from '../assets/icons8-github-60.png'
+import linkedin from '../assets/icons8-linkedin-60.png'
+import gmail from '../assets/icons8-gmail-60.png'
 
 export default class Contact extends Component {
     render() {
         return (
-            <div id="contact" className="container text-center">
+            <div id="contact" className="smoke-dev">
                 <h1 className="heading">Let's get in touch</h1>
                 <div className="line"></div>
-
-                <h4>✨😺come check out my cats😸✨</h4>
-                <img src={lucas} height="48" />
+                <div className="flex-dev container">
+                    <div>
+                        <h6>You can find my work on <a href="https://github.com/margaret-jihua">Github</a></h6>
+                        <a href="https://github.com/margaret-jihua"><img src={github} alt="github" /></a>
+                    </div>
+                    <div>
+                        <h6>Connect with me on <a href="https://www.linkedin.com/in/margaret-jihua/">LinkedIn</a></h6>
+                        <a href="https://www.linkedin.com/in/margaret-jihua/"><img src={linkedin} alt="linkedin" /></a>
+                    </div>
+                    <div>
+                        <h6>Feel free to say <a href="mailto:margaret.jihua@gmail.com">Hi</a></h6>
+                        <a href="mailto:margaret.jihua@gmail.com"><img src={gmail} alt="gmail" /></a>
+                    </div>
+                </div>
+                <h4>🐈 I'm also a cat lover 🐈</h4>
+                <h4>✨😺 come check out my cats 😸✨</h4>
+                <img src={lucas} alt="lucas" height="48" />
                 <img src={lily} alt="Lily" height="48" />
                 <a href="https://www.instagram.com/walklikeacat"><img src={instagram} height="62" /></a>
+                <footer className="text-center" style={{marginTop: "50px"}}>Design & Build by Margaret (Jihua) Huang</footer>
             </div>
         )
     }
