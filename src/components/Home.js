75956ded19689ca32bc40c56acef626de64f3e84
margare-jihua <margaret.jihua@gmail.com>
2020-10-30 16:31:44 -0700

    Add project pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
+
 import margaret from '../assets/Jihua(Margaret)Huang.pdf'
 
 export default class Home extends Component {
@@ -10,20 +11,10 @@ export default class Home extends Component {
     render() {
 
         const projects = [
-            {
-                name: "Bacon & Egg Tik Tak Toe", 
-                link: "https://margaret-jihua.github.io/tik-tak-toe/",
-                repo: "https://github.com/margaret-jihua/tik-tak-toe",
-                description: "A classic Tik-Tak-Toe game with adorable bacon & egg layout."
-            },
-            {
-                name: "The National Flag Game", 
-                link: "https://margaret-jihua.github.io/national-flag-game/",
-                repo: "https://github.com/margaret-jihua/national-flag-game",
-                description: "This is a game of identifying countries base on 4 different flags. If a player makes 5 wrong guesses, the player lose the game."
-            },
+            
             {
                 name: "The Astronomy App", 
+                img: "Astronomy",
                 link: "https://astronomy-picture-app.herokuapp.com/",
                 repo: "https://github.com/margaret-jihua/Astronomy-App",
                 tech: ["Node.js", "Express", "PostgresQL", "RESTful API"],
@@ -31,25 +22,42 @@ export default class Home extends Component {
             },
             {
                 name: "Affirmation", 
+                img: "Affirmation",
                 link: "https://affirmations-client.herokuapp.com/",
                 repo: "https://github.com/margaret-jihua/Affirmations-App",
                 tech: ["React", "MongoDB", "Node.js", "Express"],
                 description: "An open API with some Affirmation quotes and supporting resources. Users can create an account, save quotes to their profile by liking, and submitting quotes of their own. Users will have access to the crisis prevention hotline and find professional help in their area, as well as have access to chat with other users for more personal advice."
             },
             {
+                name: "Zero-G", 
+                img: "ZeroG",
+                link: "https://zero-g-frontend-25239.web.app/", 
+                repo: "https://github.com/margaret-jihua/zero-g-frontend", 
+                tech: ["React", "MongoDB", "Node.js", "Express"],
+                description: "A fitness app for future-space-travelers. In case of muscular density decreases in space, it is crucial to maintain exercise. Our app offers timer with music while users log their daily acitiveties. It's a not fully functional site, we listed possible implementations for the future."
+            },
+            {
                 name: "Off the shelf", 
+                img: "OffTheShelf",
                 link: "https://offtheshelff.herokuapp.com/",
                 repo: "https://github.com/margaret-jihua/Off-the-Shelf",
                 tech: ["Django", "PostgresQL", "API"],
                 description: "A mockup Goodreads app. Allows Users to create an account, search for books, and receive information on similar books, ratings, reviews, purchase links and film base of the book if any. Users can also save books to their wishlist and leave comments under each book."
             },
             {
-                name: "Zero-G", 
-                link: "https://zero-g-frontend-25239.web.app/", 
-                repo: "https://github.com/margaret-jihua/zero-g-frontend", 
-                tech: ["React", "MongoDB", "Node.js", "Express"],
-                description: "A fitness app for future-space-travelers. In case of muscular density decreases in space, it is crucial to maintain exercise. It's not fully functional site, we have 3 days to build this in a hackathon."
-            }
+                game: true,
+                name: "Bacon & Egg Tik Tak Toe", 
+                link: "https://margaret-jihua.github.io/tik-tak-toe/",
+                repo: "https://github.com/margaret-jihua/tik-tak-toe",
+                description: "A classic Tik-Tak-Toe game with adorable bacon & egg layout."
+            },
+            {
+                game: true,
+                name: "The National Flag Game", 
+                link: "https://margaret-jihua.github.io/national-flag-game/",
+                repo: "https://github.com/margaret-jihua/national-flag-game",
+                description: "This is a game of identifying countries base on 4 different flags. If a player makes 5 wrong guesses, the player lose the game."
+            },
         ]
 
         return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,23 +2,64 @@ import React, {Component} from 'react'
 import github from '../assets/icons8-github-48.png'
 import site from '../assets/icons8-internet-48.png'
 
+import Affirmation from '../assets/projects/Affirmation.png'
+import Astronomy from '../assets/projects/Astronomy.png'
+import OffTheShelf from '../assets/projects/off-the-shelf.png'
+import ZeroG from '../assets/projects/Zero-G.png'
+
 export default class Projects extends Component {
     render() {
-        const projectList = this.props.projects.map((p,i) => {
 
+        const getImg = (img) => {
+            switch (img) {
+                case 'Affirmation':
+                    return Affirmation
+                case 'Astronomy':
+                    return Astronomy
+                case 'OffTheShelf':
+                    return OffTheShelf
+                case 'ZeroG':
+                    return ZeroG
+            }
+        }
+
+        const projectList = this.props.projects.map((p,i) => {           
             return (
-                <div className="card col-sm-12 col-md-6 col-lg-4">
-                    <div className="card-body">
-                        <h5 className="card-title">{p.name}</h5>
-                        <div className="card-subtitle text-muted d-flex flex-wrap">
+                p.game ? ('') : (
+                <div className="col-sm-12 col-md-6" style={{padding:"20px"}}>
+                    {/* <div className=""> */}
+                        {/* <div className=""> */}
+                        <img src={getImg(p.img)} alt={p.img} style={{height:"270px", width:"540px", objectFit:"scale-down", marginBottom:"20px"}}/> 
+                        {/* </div> */}
+                        <h5 className="">{p.name}</h5>
+                        <div className="text-muted d-flex flex-wrap">
                             {p.tech ? (p.tech.map((t, i) => <span style={{marginRight:"10px"}}>{t}</span>)) : ('')}
-                            {/* {p.tech} */}
                         </div>
-                        <p className="card-text">{p.description}</p>
+                        <p className="">{p.description}</p>
                         <a href={p.link} className="card-link"><img src={site} /></a>
                         <a href={p.repo} className="card-link"><img src={github} /></a>
+                    {/* </div> */}
+                </div>
+                )
+            )
+        })
+
+        const games = this.props.projects.map((p,i) => {           
+            return (
+                p.game ? (
+                <div className="d-flex flex-wrap">
+                    <div className="">
+                        <img className="" src={getImg(p.img)} alt={p.img} style={{height:"270px", width:"", objectFit:"scale-down", marginBottom:"20px"}}/> 
+                        <h5 className="">{p.name}</h5>
+                        <div className="text-muted d-flex flex-wrap">
+                            {p.tech ? (p.tech.map((t, i) => <span style={{marginRight:"10px"}}>{t}</span>)) : ('')}
+                        </div>
+                        <p className="">{p.description}</p>
+                        <a href={p.link} className=""><img src={site} /></a>
+                        <a href={p.repo} className=""><img src={github} /></a>
                     </div>
                 </div>
+                ) : ('')
             )
         })
 
